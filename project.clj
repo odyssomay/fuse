644a7de4a0d856893ccdb060f03c5658fc01a597
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-04-22 22:33:51 +0200

    added watchtower to deps

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "FIXME: write description"
   :eval-in-leiningen true
   :dependencies [[org.fusesource.jansi/jansi "1.9"]
-                 [org.eclipse.jgit "2.3.1.201302201838-r"]])
+                 [org.eclipse.jgit "2.3.1.201302201838-r"]
+                 [watchtower "1.3.0"]])
